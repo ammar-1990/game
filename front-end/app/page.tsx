c71ac485370885cb16460f118880d030ae347a47
ammar <ammar.ali.haidar.1990@gmail.com>
2024-06-30 21:12:33 +0400

    done

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -3,10 +3,10 @@ import MainView from "./components/main-view";
 
 export default function Home() {
   return (
-   <main className="min-h-screen w-full bg-gradient-to-br from-gray-900 to-gray-800 pt-8">
+   <main className="min-h-screen w-full bg-gradient-to-br from-gray-900 to-gray-800 pt-8 overflow-x-auto scroll">
     
     
-    <div className="max-w-[1100px] mx-auto pb-4">
+    <div className="max-w-[1100px] mx-auto pb-4 min-w-[900px]">
     <MainView/>
     </div>
 
